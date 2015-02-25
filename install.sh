9f41693fd42f7728029714906e60c4e19aaada20
mnpk <mwpark@castis.com>
2015-02-25 17:58:48 +0900

    update install script

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
-echo "installing... cpm to /usr/local/bin/"
-install cpm.py /usr/local/bin/cpm
+#!/bin/bash -e
+echo "installing cpm..."
+mkdir ~/.cpm
+git clone --recursive https://github.com/castisdev/cpm.git ~/.cpm/cpm
+sudo ln -sf ~/.cpm/cpm/cpm.py /usr/local/bin/cpm
 echo "...done."
