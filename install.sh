f98467cf86b264d43af7172fdba7761d3fd3f5a0
mnpk <mwpark@castis.com>
2015-02-26 13:35:15 +0900

    install script 수정

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 echo "installing cpm..."
 mkdir -p ~/.cpm/bin
-git clone --recursive https://github.com/castisdev/cpm.git ~/.cpm/cpm
-ln -sf ~/.cpm/cpm/cpm.py ~/.cpm/bin/cpm
+git clone --recursive https://github.com/castisdev/cpm.git ~/.cpm/src/cpm
+ln -sf ~/.cpm/src/cpm/cpm.py ~/.cpm/bin/cpm
 echo 'export PATH="~/.cpm/bin:$PATH"' >> ~/.bashrc
-source ~/.bashrc
+export PATH="~/.cpm/bin:$PATH"
 echo "...done."
